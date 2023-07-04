4ecf8b1d8d406aa63246c90f357d32879f402caa
hushiqi <1294337509@qq.com>
2023-07-05 01:15:12 +0800

    chore: optimize code of tree chain split

--- 图论/树链剖分/树链剖分.cpp
+++ 图论/树链剖分/树链剖分.cpp
@@ -1,80 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct ChainSplit
-{
-    const static int N = 1e5; //node[1, N]
-    int par[N + 5], dep[N + 5], siz[N + 5], son[N + 5], top[N + 5], dfn[N + 5];
-    int dfsc;
-    void split(vector<int> g[], int n)
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            par[i] = 0;
-            dep[i] = 0;
-            siz[i] = 0;
-            son[i] = 0;
-            top[i] = 0;
-            dfn[i] = 0;
-        }
-        dfsc = 0;
-        dfs1(1, 0, g);
-        dfs2(1, 0, 1, g);
+
+struct TreeChainSpliter {
+    struct node {
+        int par;  // parent node
+        int dep;  // deep
+        int siz;  // size
+        int hvy;  // heavy child
+        int top;  // top node of chain
+        int dfo;  // dfs order
+    };
+
+    int dfs_stamp;  // dfs stamp
+    vector<int> *g;  // origin tree
+    vector<node> nodes;  // splited tree
+
+    void init(int n, vector<int> g[]) {
+        this->g = g;
+        nodes = vector<node>(n + 1);
     }
-    void dfs1(int now, int pre, vector<int> g[])
-    {
-        son[now] = 0;
-        siz[now] = 1;
-        par[now] = pre;
-        dep[now] = dep[pre] + 1;
-        for (int i : g[now])
-            if (i != pre)
-            {
-                dfs1(i, now, g);
-                siz[now] += siz[i];
-                if (siz[i] > siz[son[now]])
-                    son[now] = i;
-            }
+
+    void split(int root) {
+        dfs_stamp = 0;
+        dfs1(root, 0);
+        dfs2(root, 0, root);
     }
-    void dfs2(int now, int pre, int tp, vector<int> g[])
-    {
-        top[now] = tp;
-        dfn[now] = ++dfsc;
-        if (son[now])
-            dfs2(son[now], now, tp, g);
-        for (int i : g[now])
-            if (i != pre && i != son[now])
-                dfs2(i, now, i, g);
-    }
-    void query(int x, int y)
-    {
-        int tx = top[x];
-        int ty = top[y];
-        while (tx != ty)
-        {
-            if (dep[tx] >= dep[ty])
-            {
-                //询问[dfn[tx], dfn[x]]
-                x = par[tx];
-                tx = top[x];
-            }
-            else
-            {
-                //询问[dfn[ty], dfn[y]]
-                y = par[ty];
-                ty = top[y];
+
+    // calculate hvy, siz, par, dep
+    void dfs1(int now, int pre) {
+        nodes[now].hvy = 0;
+        nodes[now].siz = 1;
+        nodes[now].par = pre;
+        nodes[now].dep = nodes[pre].dep + 1;
+        for (int nxt : g[now]) {
+            if (nxt != pre) {
+                dfs1(nxt, now);
+                nodes[now].siz += nodes[nxt].siz;
+                if (nodes[nxt].siz > nodes[nodes[now].hvy].siz) {
+                    nodes[now].hvy = nxt;
+                }
             }
         }
-        if (dep[x] >= dep[y])
-        {
-            //询问[dfn[y], dfn[x]]
+    }
+
+    // calculate top, dfo
+    void dfs2(int now, int pre, int top) {
+        nodes[now].top = top;
+        nodes[now].dfo = ++dfs_stamp;
+        if (nodes[now].hvy) {
+            dfs2(nodes[now].hvy, now, top);
         }
-        else
-        {
-            //询问[dfn[x], dfn[y]]
+        for (int nxt : g[now]) {
+            if (nxt != pre && nxt != nodes[now].hvy) {
+                dfs2(nxt, now, nxt);
+            }
         }
+    }
+
+    // query path bewteen two node
+    int query(int x, int y) {
+        int ans = 0;  // answer
+
+        int top_x = nodes[x].top;
+        int top_y = nodes[y].top;
+        while (top_x != top_y) {
+            if (nodes[top_x].dep >= nodes[top_y].dep) {
+                ans += _query(nodes[top_x].dfo, nodes[x].dfo);
+                x = nodes[top_x].par;
+                top_x = nodes[x].top;
+            } else {
+                ans += _query(nodes[top_y].dfo, nodes[y].dfo);
+                y = nodes[top_y].par;
+                top_y = nodes[y].top;
+            }
+        }
+        if (nodes[x].dep >= nodes[y].dep) {
+            ans += _query(nodes[x].dfo, nodes[y].dfo);
+        } else {
+            ans += _query(nodes[y].dfo, nodes[x].dfo);
+        }
+        return ans;
+    }
+
+    int _query(int l, int r) {
+        // customize your query here or directly modify the _query above
+        return 0;
     }
 };
-int main()
-{
+
+int main() {
     return 0;
 }
